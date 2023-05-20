851131b87d65565f1c2e6508dd4abec47737e25f
Sunkireddy.Javahar Reddy <89154426+javaharreddy@users.noreply.github.com>
2023-05-20 22:54:34 +0530

    Create Best Time to Buy and Sell Stock.py

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,7 @@
+def maxProfit(self, prices: List[int]) -> int:
+        mini=prices[0]
+        profit=0
+        for i in range(1,len(prices)):
+            profit=max(profit,prices[i]-mini)
+            mini=min(mini,prices[i])
+        return profit
